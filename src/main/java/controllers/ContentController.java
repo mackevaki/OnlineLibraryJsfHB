131d77bef12cd6fa5f866ad1c5b27b563a4e5b79
mackevaki <sshohus@gmail.com>
2022-12-13 14:11:22 +0300

    adeed opportunity to change books images

---
+++ src/main/java/controllers/ContentController.java
@@ -0,0 +1,67 @@
+package controllers;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.event.ActionEvent;
+import jakarta.faces.event.ActionListener;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import java.io.Serializable;
+
+import org.primefaces.event.FileUploadEvent;
+
+@Named(value = "contentController")
+@SessionScoped
+public class ContentController implements Serializable {
+    private byte[] uploadedContent;
+    private boolean showContent;
+    
+    @Inject
+    private BookListController bookListController;
+
+    public BookListController getBookListController() {
+        return bookListController;
+    }
+
+    public void setBookListController(BookListController bookListController) {
+        this.bookListController = bookListController;
+    }
+
+    public void handleFileUpload(FileUploadEvent event) {
+        uploadedContent = event.getFile().getContent();
+        if (uploadedContent != null && uploadedContent.length > 0){
+            showContent = true;
+        }
+        bookListController.getSelectedBook().setContent(uploadedContent);
+    }
+
+//    public ActionListener saveListener() {
+//        return (ActionEvent event) -> {
+//            if (uploadedContent != null) {
+//                bookListController.getSelectedBook().setContent(uploadedContent);
+//                bookListController.getSelectedBook().setContentEdited(true);
+//            }
+//            clear();
+//        };
+//    }
+
+    public ActionListener clearListener() {
+        return (ActionEvent event) -> {
+            clear();
+        };
+    }
+
+    public void clear() {
+        uploadedContent = null;
+        showContent = false;
+    }
+
+    public boolean isShowContent() {
+        return showContent;
+    }
+
+    public byte[] getUploadedContent() {
+        return uploadedContent;
+    }
+}
+
+
---
+++ src/main/java/controllers/ImageController.java
@@ -0,0 +1,92 @@
+package controllers;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.event.ActionEvent;
+import jakarta.faces.event.ActionListener;
+import jakarta.inject.Inject;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+
+@Named(value = "imageController")
+@SessionScoped
+public class ImageController implements Serializable {
+    private final int IMAGE_MAX_SIZE = 204800;
+    private byte[] uploadedImage;
+    
+    @Inject
+    private BookListController bookListController;
+    
+    public ImageController() {
+    }
+
+    public BookListController getBookListController() {
+        return bookListController;
+    }
+
+    public void setBookListController(BookListController bookListController) {
+        this.bookListController = bookListController;
+    }
+    
+    public StreamedContent getDefaultImage() {
+        return getStreamedContent(bookListController.getSelectedBook().getImage());
+    }
+
+    public StreamedContent getUploadedImage() {
+        return getStreamedContent(uploadedImage);
+    }
+
+    public void handleFileUpload(FileUploadEvent event) {
+        uploadedImage = event.getFile().getContent();
+        bookListController.getSelectedBook().setImage(uploadedImage);
+    }
+    
+    private DefaultStreamedContent getStreamedContent(byte[] image) {
+        if (image == null) {
+            return null;
+        }
+
+        try(InputStream inputStream = new ByteArrayInputStream(image);) {
+            return DefaultStreamedContent.builder().contentType("image/png").stream(() -> inputStream).build();
+        } catch (IOException ex) {
+            Logger.getLogger(ImageController.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return null;
+    }
+    
+    public int getImageMaxSize() {
+        return IMAGE_MAX_SIZE;
+    }
+
+    public byte[] getUploadedImageBytes() {
+        return uploadedImage;
+    }
+
+//    public ActionListener saveListener() {
+//        return (ActionEvent event) -> {
+//            if (uploadedImage != null) {
+//                bookListController.getSelectedBook().setImage(uploadedImage);
+//                bookListController.getSelectedBook().setImageEdited(true);
+//            }
+//            clear();
+//        };
+//    }
+    
+    public ActionListener clearListener() {
+        return (ActionEvent event) -> {
+            clear();
+        };
+    }
+
+    public void clear() {
+        uploadedImage = null;
+    }    
+}
